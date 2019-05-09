b37a93c4f3132b5eadb1c837da7ba57eabb0496f
Enrique Tun <enrique.tun@adxpace.com>
2019-05-09 17:19:29 -0500

    SE creo el token de autentificacion

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import CrearUsuarioView, CrearTokenView
+from .views import CrearUsuarioView, CrearTokenView, AdministrarUsuarioView
 
 app_name = 'user'
 
 urlpatterns = [
     path('create/', CrearUsuarioView.as_view(), name='create'),
     path('token/', CrearTokenView.as_view(), name='token'),
+    path('me/', AdministrarUsuarioView.as_view(), name='me'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,6 @@
 
-from rest_framework.generics import CreateAPIView
+from rest_framework import authentication, permissions
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -14,3 +15,15 @@ class CrearTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class AdministrarUsuarioView(RetrieveUpdateAPIView):
+    """manage the authenticated user"""
+    serializer_class = UsuarioSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """REtrive and return aunthentication"""
+
+        return self.request.user
