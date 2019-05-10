b86cd44b5d105f65bf84a371a75747637dc4761b
Enrique Tun <enrique.tun@adxpace.com>
2019-05-10 16:31:12 -0500

    Se agrego el serializer para nuevos ingredientes

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -24,7 +24,8 @@ class TagViewSet(viewsets.GenericViewSet,
         serializer.save(user=self.request.user)
 
 class IngredienteViewSet(viewsets.GenericViewSet, 
-                         mixins.ListModelMixin):
+                         mixins.ListModelMixin,
+                         mixins.CreateModelMixin):
     """Manage ingrediebntes in the database"""
 
     authentication_classes = (TokenAuthentication,)
@@ -36,3 +37,7 @@ class IngredienteViewSet(viewsets.GenericViewSet,
     def get_queryset(self):
         """return objects for the current authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+    
+    def perform_create(self, serializer):
+        """Create a new ingrediente"""
+        serializer.save(user=self.request.user)
